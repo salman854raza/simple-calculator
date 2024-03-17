d35197f47d6c1eee08ee85c41e548456bf30c830
Salman786Raza <salman854raza@gmail.com>
2024-03-17 05:08:29 -0700

    first Assignmet

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter Your First Number", type: "number", name: "FirstNumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of operators to perform action ",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+//   CONDITIONAL STATEMENT
+if (answer.operator === "ADDITION") {
+    console.log(answer.FirstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.FirstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.FirstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.FirstNumber / answer.secondNumber);
+}
+else {
+    "please select correct operator";
+}
---
+++ index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter Your First Number", type: "number", name: "FirstNumber" },
+  { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of operators to perform action ",
+    type: "list",
+    name: "operator",
+    choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+  },
+]);
+//   CONDITIONAL STATEMENT
+if (answer.operator === "ADDITION"){
+    console.log(answer.FirstNumber + answer.secondNumber);
+} else if (answer.operator === "SUBTRACTION"){
+    console.log(answer.FirstNumber - answer.secondNumber);
+} else if (answer.operator === "MULTIPLICATION"){
+    console.log(answer.FirstNumber * answer.secondNumber);
+} else if (answer.operator === "DIVISION"){
+    console.log(answer.FirstNumber / answer.secondNumber);
+}
+else{"please select correct operator"}
+
